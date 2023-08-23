a2c019b184989be60667864bb14746b2c1acde0f
Infirmax <77449439+Infirmax@users.noreply.github.com>
2023-08-22 22:29:27 -0400

    svelte version by andy

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	let newTaskName;
+	let tasks = [];
+
+	function addTask() {
+		tasks.push(newTaskName);
+		tasks = tasks;
+	}
+
+	function deleteTask(event) {
+		event.srcElement.parentNode.remove();
+	}
+
+	function saveTasks() {
+		localStorage.setItem("tasks", tasks.join("|"));
+	}
+
+	function loadTasks() {
+		tasks = localStorage.getItem("tasks").split("|");
+	}
+</script>
+
+<svelte:window on:load={loadTasks}/>
+<h1>To Do List</h1>
+
+<input type="text" id="task" bind:value={newTaskName} />
+<input type="button" value="Add Task" on:click={addTask} />
+
+<h1>List of Tasks</h1>
+<ul id="tasks">
+	{#each tasks as task}
+		<div>
+			<li>{task}</li>
+			<button on:click={deleteTask}>Delete me</button>
+		</div>
+	{/each}
+</ul>
+
+<style>
+</style>
